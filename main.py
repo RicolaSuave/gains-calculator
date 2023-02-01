6a16116515ba7dcfa26761aa2b7d582a60da920c
Rico Laureano <ricomateo711@gmail.com>
2023-02-01 14:31:38 -0800

    Generate in 3s

--- main.py
+++ main.py
@@ -15,82 +15,89 @@ with open("config.yaml", "r") as file:
 # 5.   add onto muscle groups that we have covered, all of the muscle groups for that item
 # 6. make sure to cover the minimum number of wkouts/day
 
-input_workout_type = input("Enter 'push', 'pull', or 'legs': ")
+input_workdays = int(input("Input # of days you would like to workout: "))   
+input_workout_type = input("Enter starting workout, 'push', 'pull', or 'legs': ")
+
 
 if input_workout_type == "legs":
-    input_code = 0
+    input_code = -1
 elif input_workout_type == "push":
-    input_code = 1
+    input_code = 0
 elif input_workout_type == "pull":
-    input_code = 2
+    input_code = 1
 
-current_workout = data["Workouts"][input_code]
 
-# set is a list with unique items
-muscle_groups = set(current_workout["Possible Muscle Groups"])
+max_value = 3
+for i in range((input_workdays)):
+    input_code = (input_code + 1) % max_value
 
-schedule = list()
+    current_workout = data["Workouts"][input_code]
 
-# 0. Start schedule with required exercises
-used_muscle_groups = set()
-for exercise in current_workout["List"]:
-    if exercise.get("Important"):
-        schedule.append( (exercise["Name"], exercise["Sets"]) )
-        exercise["Used"] = True
-        used_muscle_groups = used_muscle_groups.union(exercise["Muscle Groups"])
+    # set is a list with unique items
+    muscle_groups = set(current_workout["Possible Muscle Groups"])
 
-# 1. While we do not cover all muscle groups
-while muscle_groups != used_muscle_groups:
-    # 2.   get a list of unused "remaining" exercises
-    remaining_exercises = list()
+    schedule = list()
+
+    # 0. Start schedule with required exercises
+    used_muscle_groups = set()
     for exercise in current_workout["List"]:
-        if exercise.get("Used"):
-            continue
-        # skip if we have used *all* of the muscle groups of this exercise before
-        # >>> bigger = set( [1,2,3] ) 
-        # >>> subset = set( [1,2] )
-        # >>> bigger.intersection(subset)
-        # {1, 2}
-        # >>> bigger.intersection(subset) == subset
-        # True # all the items in subset are also in bigger, we can skip these
-        if used_muscle_groups.intersection(exercise["Muscle Groups"]) == set(exercise["Muscle Groups"]):
-            continue
-        remaining_exercises.append( (exercise["Name"], exercise["Sets"]) ) # "tuple" of 2 items: (__str__, str)
-    
-    # 3.   shuffle it
+        if exercise.get("Important"):
+            schedule.append( (exercise["Name"], exercise["Sets"]) )
+            exercise["Used"] = True
+            used_muscle_groups = used_muscle_groups.union(exercise["Muscle Groups"])
+
+    # 1. While we do not cover all muscle groups
+    while muscle_groups != used_muscle_groups:
+        # 2.   get a list of unused "remaining" exercises
+        remaining_exercises = list()
+        for exercise in current_workout["List"]:
+            if exercise.get("Used"):
+                continue
+            # skip if we have used *all* of the muscle groups of this exercise before
+            # >>> bigger = set( [1,2,3] ) 
+            # >>> subset = set( [1,2] )
+            # >>> bigger.intersection(subset)
+            # {1, 2}
+            # >>> bigger.intersection(subset) == subset
+            # True # all the items in subset are also in bigger, we can skip these
+            if used_muscle_groups.intersection(exercise["Muscle Groups"]) == set(exercise["Muscle Groups"]):
+                continue
+            remaining_exercises.append( (exercise["Name"], exercise["Sets"]) ) # "tuple" of 2 items: (__str__, str)
+        
+        # 3.   shuffle it
+        shuffle(remaining_exercises)
+
+        # 4.   get first item and add to schedule
+        schedule.append(remaining_exercises[0])
+
+        # 5.  add onto muscle groups that we have covered, all of the muscle groups for that item
+        # 5a. search all exercises for this item name
+        for exercise in current_workout["List"]:
+            # remaining_exercises: list[(STR,str)]
+            # remaining_exercises[0]: (STR,str)
+            # remaining_exercises[0][0]: STR
+            name = remaining_exercises[0][0]
+            if exercise["Name"] == name:
+                # 5b. concatenate (add sets together) all muscle groups for that item into used_muscle_groups (union is to add distinct entries to a list)
+                used_muscle_groups = used_muscle_groups.union(exercise["Muscle Groups"])
+                exercise["Used"] = True
+
+
+    # dict[key] throws an error if key does not exist
+    # dict.get(key) does not even if the key does not exist 
+
+    # 6. make sure to cover the minimum number of wkouts/day
+    remaining_exercises = [ (exercise["Name"], exercise["Sets"])
+                            for exercise in current_workout["List"] 
+                            if not exercise.get("Used") ]
+
     shuffle(remaining_exercises)
 
-    # 4.   get first item and add to schedule
-    schedule.append(remaining_exercises[0])
+    if len(schedule) < data["Options"]["NumberOfWorkoutsPerDay"]:
+        number_of_exercises_to_add = data["Options"]["NumberOfWorkoutsPerDay"] - len(schedule)
+        schedule += remaining_exercises[ 0 : number_of_exercises_to_add ]
 
-    # 5.  add onto muscle groups that we have covered, all of the muscle groups for that item
-    # 5a. search all exercises for this item name
-    for exercise in current_workout["List"]:
-        # remaining_exercises: list[(STR,str)]
-        # remaining_exercises[0]: (STR,str)
-        # remaining_exercises[0][0]: STR
-        name = remaining_exercises[0][0]
-        if exercise["Name"] == name:
-            # 5b. concatenate (add sets together) all muscle groups for that item into used_muscle_groups (union is to add distinct entries to a list)
-            used_muscle_groups = used_muscle_groups.union(exercise["Muscle Groups"])
-            exercise["Used"] = True
+    # Done
 
-
-# dict[key] throws an error if key does not exist
-# dict.get(key) does not even if the key does not exist 
-
-# 6. make sure to cover the minimum number of wkouts/day
-remaining_exercises = [ (exercise["Name"], exercise["Sets"])
-                        for exercise in current_workout["List"] 
-                        if not exercise.get("Used") ]
-
-shuffle(remaining_exercises)
-
-if len(schedule) < data["Options"]["NumberOfWorkoutsPerDay"]:
-    number_of_exercises_to_add = data["Options"]["NumberOfWorkoutsPerDay"] - len(schedule)
-    schedule += remaining_exercises[ 0 : number_of_exercises_to_add ]
-
-# Done
-
-pprint(schedule)
+    pprint(schedule)
 
